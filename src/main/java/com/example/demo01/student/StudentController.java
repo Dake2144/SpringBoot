b407ea31eacbd2b118713ee47c92876b48b728eb
Daulet <sametbay.i5@gmail.com>
2021-08-12 17:18:47 +0600

    put and delete 12.08.21

--- src/main/java/com/example/demo01/student/StudentController.java
+++ src/main/java/com/example/demo01/student/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
@@ -16,7 +17,7 @@ public class StudentController {
     }
 
     @GetMapping
-    public List<Student> getStudents (){
+    public List<Student> getStudents() {
         return studentService.getStudents();
     }
 
@@ -24,4 +25,19 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+
+        studentService.updateStudent(studentId, name, email);
+    }
+
 }
